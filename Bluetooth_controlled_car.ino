3d015a6a41605bcac37582d79df565d844474f06
Jefferson <128813232+Jeffersonben@users.noreply.github.com>
2025-06-17 02:28:34 +0530

    Add files via upload

---
+++ Bluetooth_controlled_car.ino
@@ -0,0 +1,103 @@
+//Arduino Bluetooth Controlled Car
+//Before uploading the code you have to install the necessary library
+//Note - Disconnect the Bluetooth Module before hitting the upload button otherwise you'll get a compilation error message.
+
+//Now You Can Upload the Code without any problem but make sure the Bluetooth module isn't connected to Arduino while uploading the code
+
+#include <AFMotor.h>
+
+//initial motors pin
+AF_DCMotor motor1(1, MOTOR12_1KHZ); 
+AF_DCMotor motor2(2, MOTOR12_1KHZ); 
+AF_DCMotor motor3(3, MOTOR34_1KHZ);
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
+
+char command; 
+
+void setup() 
+{       
+  Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
+}
+
+void loop(){
+  if(Serial.available() > 0){  // for the stop we intialize some number greater than 0 it triggers the Stop() function    
+    command = Serial.read(); 
+    Stop(); //initialize with motors stoped
+    //Change pin mode only if the new command is different from previous one.   
+    //Serial.println(command);
+    switch(command){
+    case 'F':  
+      forward();
+      break;
+    case 'B':  
+       back();
+      break;
+    case 'L':  
+      left();
+      break;
+    case 'R':
+      right();
+      break;
+    }
+  } 
+}
+
+void forward()
+{
+  motor1.setSpeed(255); //Define maximum velocity
+  motor1.run(FORWARD); //rotate the motor clockwise
+  motor2.setSpeed(255); //Define maximum velocity
+  motor2.run(FORWARD); //rotate the motor clockwise
+  motor3.setSpeed(255);//Define maximum velocity
+  motor3.run(FORWARD); //rotate the motor clockwise
+  motor4.setSpeed(255);//Define maximum velocity
+  motor4.run(FORWARD); //rotate the motor clockwise
+}
+
+void back()
+{
+  motor1.setSpeed(255); //Define maximum velocity
+  motor1.run(BACKWARD); //rotate the motor anti-clockwise
+  motor2.setSpeed(255); //Define maximum velocity
+  motor2.run(BACKWARD); //rotate the motor anti-clockwise
+  motor3.setSpeed(255); //Define maximum velocity
+  motor3.run(BACKWARD); //rotate the motor anti-clockwise
+  motor4.setSpeed(255); //Define maximum velocity
+  motor4.run(BACKWARD); //rotate the motor anti-clockwise
+}
+
+void left()
+{
+  motor1.setSpeed(255); //Define maximum velocity
+  motor1.run(BACKWARD); //rotate the motor anti-clockwise
+  motor2.setSpeed(255); //Define maximum velocity
+  motor2.run(BACKWARD); //rotate the motor anti-clockwise
+  motor3.setSpeed(255); //Define maximum velocity
+  motor3.run(FORWARD);  //rotate the motor clockwise
+  motor4.setSpeed(255); //Define maximum velocity
+  motor4.run(FORWARD);  //rotate the motor clockwise
+}
+
+void right()
+{
+  motor1.setSpeed(255); //Define maximum velocity
+  motor1.run(FORWARD); //rotate the motor clockwise
+  motor2.setSpeed(255); //Define maximum velocity
+  motor2.run(FORWARD); //rotate the motor clockwise
+  motor3.setSpeed(255); //Define maximum velocity
+  motor3.run(BACKWARD); //rotate the motor anti-clockwise
+  motor4.setSpeed(255); //Define maximum velocity
+  motor4.run(BACKWARD); //rotate the motor anti-clockwise
+} 
+
+void Stop()
+{
+  motor1.setSpeed(0); //Define minimum velocity
+  motor1.run(RELEASE); //stop the motor when release the button
+  motor2.setSpeed(0); //Define minimum velocity
+  motor2.run(RELEASE); //rotate the motor clockwise
+  motor3.setSpeed(0); //Define minimum velocity
+  motor3.run(RELEASE); //stop the motor when release the button
+  motor4.setSpeed(0); //Define minimum velocity
+  motor4.run(RELEASE); //stop the motor when release the button
+}
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# StairClimbingDevice
+# Stair Climbing Device
+
+This repository contains the design and implementation details for a Remote-Controlled Stair-Climbing Robot. The robot employs a Rocker-Bogie Suspension Mechanism inspired by NASA's Mars Rovers, enabling it to climb stairs and traverse rugged terrains effectively. It is controlled via Bluetooth and built using economical and readily available components.
+
+## Overview
+-Stair-climbing capability using the Rocker-Bogie mechanism.
+-Remote control via Bluetooth.
+
+
+The **Rocker Bogie Suspension Vehicle** is a type of rover widely used in planetary exploration, such as on NASA’s Mars rovers. The vehicle’s suspension system allows it to traverse rough terrain, and it is commonly used in robotics competitions and learning projects.
+
+### Key Features:
+- **Rocker Bogie Suspension**: Enables smooth traversal over uneven surfaces.
+- **Arduino-controlled**: The movement of the vehicle is managed using an Arduino board.
+- **Simple Circuitry**: Includes motor control circuits and wiring for the vehicle's movement.
+
+
+The **Rocker Bogie Suspension System** works by:
+- Distributing the vehicle’s weight across six wheels.
+- Allowing the vehicle to climb stairs from varying height 3cm-25cm.
+- Keeping the body of the vehicle stable.
+
+
+While working on this project, I learned:
+- Basics of **Arduino programming** and its integration with motors.
+- Designing **simple circuits** and troubleshooting electrical components.
+- Building a mechanically sound suspension system and understanding its real-world applications.
+
+
+## Technologies Used
+
+- **Arduino**: For controlling the vehicle and managing input/output operations.
+- **DC Motors**: To drive the wheels.
+- **Mechanical Design**: Rocker bogie suspension setup for rough terrain adaptability.
+- **Adafruit-Motor-Sheild-Library**: Control Motor's speed.
